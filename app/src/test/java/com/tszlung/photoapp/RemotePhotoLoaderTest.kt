b5ce8ea2debf0926b73f8c684da3786163e33d8c
tzc1234 <peopli9997@gmail.com>
2024-09-30 16:59:15 +0100

    Add makeSUT helper

--- app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
@@ -8,23 +8,28 @@ class RemotePhotoLoaderTest {
     @Test
     @Suppress("UNUSED_VARIABLE")
     fun `does not notify client upon init`() {
-        val client = HTTPClientSpy()
-        val url = URL("https://a-url.com")
-        val sut = RemotePhotoLoader(client = client, url = url)
+        val (sut, client) = makeSUT()
 
         assertTrue(client.messages.isEmpty())
     }
 
     @Test
     fun `request URL from client`() {
-        val client = HTTPClientSpy()
         val url = URL("https://a-url.com")
-        val sut = RemotePhotoLoader(client = client, url = url)
+        val (sut, client) = makeSUT(url = url)
 
         sut.load()
 
         assertEquals(client.messages, listOf(url))
     }
+
+    // region Helpers
+    private fun makeSUT(url: URL = URL("https://any-url.com")): Pair<RemotePhotoLoader, HTTPClientSpy> {
+        val client = HTTPClientSpy()
+        val sut = RemotePhotoLoader(client = client, url = url)
+        return Pair(sut, client)
+    }
+    // endregion
 }
 
 class RemotePhotoLoader(private val client: HTTPClientSpy, private val url: URL) {
