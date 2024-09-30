cf950d58c6a177a27d4a5b4b01ef35d6bc970153
tzc1234 <peopli9997@gmail.com>
2024-09-30 16:50:25 +0100

    Request URL from client

--- app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
@@ -2,22 +2,41 @@ package com.tszlung.photoapp
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.net.URL
 
 class RemotePhotoLoaderTest {
     @Test
     @Suppress("UNUSED_VARIABLE")
     fun `does not notify client upon init`() {
         val client = HTTPClientSpy()
-        val sut = RemotePhotoLoader(client = client)
+        val url = URL("https://a-url.com")
+        val sut = RemotePhotoLoader(client = client, url = url)
 
         assertTrue(client.messages.isEmpty())
     }
+
+    @Test
+    fun `request URL from client`() {
+        val client = HTTPClientSpy()
+        val url = URL("https://a-url.com")
+        val sut = RemotePhotoLoader(client = client, url = url)
+
+        sut.load()
+
+        assertEquals(client.messages, listOf(url))
+    }
 }
 
-class RemotePhotoLoader(private val client: HTTPClientSpy) {
-
+class RemotePhotoLoader(private val client: HTTPClientSpy, private val url: URL) {
+    fun load() {
+        client.getFor(url)
+    }
 }
 
 class HTTPClientSpy {
-    val messages = mutableListOf<Any>()
+    val messages = mutableListOf<URL>()
+
+    fun getFor(url: URL) {
+        messages.add(url)
+    }
 }
