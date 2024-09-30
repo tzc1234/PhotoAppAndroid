fa7b219b8536fd89cdf47c51394ab46e724f1432
tzc1234 <peopli9997@gmail.com>
2024-09-30 17:55:31 +0100

    Move Error and Result to production

---
+++ app/src/main/java/com/tszlung/photoapp/features/Error.kt
@@ -0,0 +1,4 @@
+package com.tszlung.photoapp.features
+
+interface Error {
+}
--- app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
 import java.net.URL
+import com.tszlung.photoapp.features.Error
+import com.tszlung.photoapp.features.Result
 
 class RemotePhotoLoaderTest {
     @Test
@@ -67,10 +69,4 @@ class HTTPClientSpy(private val error: Error) {
         messages.add(url)
         return Result.Failure(error)
     }
-}
-
-sealed interface Error
-sealed interface Result<out D, out E : Error> {
-    data class Success<out D, out E : Error>(val data: D) : Result<D, E>
-    data class Failure<out D, out E : Error>(val error: E) : Result<D, E>
 }
