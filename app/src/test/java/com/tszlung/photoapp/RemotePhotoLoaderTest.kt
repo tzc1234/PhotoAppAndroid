a51fc28052d4f571c738a9899e1e5c6c6cabeebc
tzc1234 <peopli9997@gmail.com>
2024-10-01 10:56:39 +0100

    RemotePhotoLoader delivers photos on photos data

--- app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/RemotePhotoLoaderTest.kt
@@ -6,7 +6,15 @@ import org.junit.jupiter.api.fail
 import java.net.URL
 import com.tszlung.photoapp.features.*
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 
 class RemotePhotoLoaderTest {
@@ -38,7 +46,7 @@ class RemotePhotoLoaderTest {
                 result.error
             )
 
-            is Result.Success -> fail("should not be success here")
+            is Result.Success -> fail("Should not be success here")
         }
     }
 
@@ -53,7 +61,7 @@ class RemotePhotoLoaderTest {
                 result.error
             )
 
-            is Result.Success -> fail("should not be success here")
+            is Result.Success -> fail("Should not be success here")
         }
     }
 
@@ -63,8 +71,19 @@ class RemotePhotoLoaderTest {
         val (sut, _) = makeSUT(stub = Result.Success(emptyPhotoData))
 
         when (val result = sut.load()) {
-            is Result.Failure -> fail("should not be failure here")
+            is Result.Failure -> fail("Should not be failure here")
             is Result.Success -> assertTrue(result.data.isEmpty())
+        }
+    }
+
+    @Test
+    fun `delivers photos on photos data`() = runBlocking {
+        val photos = listOf(makePhoto(0), makePhoto(1), makePhoto(2))
+        val (sut, _) = makeSUT(stub = Result.Success(photos.toData()))
+
+        when (val result = sut.load()) {
+            is Result.Failure -> fail("Should not be failure here")
+            is Result.Success -> assertEquals(photos, result.data)
         }
     }
 
@@ -77,6 +96,29 @@ class RemotePhotoLoaderTest {
         val sut = RemotePhotoLoader(client = client, url = url)
         return Pair(sut, client)
     }
+
+    private fun makePhoto(index: Int) = Photo(
+        id = index.toString(),
+        author = "author$index",
+        width = index,
+        height = index,
+        webURL = URL("https://web-url-$index.com"),
+        imageURL = URL("https://url-$index.com")
+    )
+
+    private fun List<Photo>.toData(): ByteArray {
+        val response = map {
+            PhotoResponse(
+                id = it.id,
+                author = it.author,
+                width = it.width,
+                height = it.height,
+                url = it.webURL,
+                downloadURL = it.imageURL
+            )
+        }
+        return Json.encodeToString(response).toByteArray(Charsets.UTF_8)
+    }
     // endregion
 }
 
@@ -86,14 +128,24 @@ class RemotePhotoLoader(private val client: HTTPClientSpy, private val url: URL)
         INVALID_DATA
     }
 
-     suspend fun load(): Result<List<Photo>, Error> {
+    suspend fun load(): Result<List<Photo>, Error> {
         return when (val result = client.getFor(url)) {
             is Result.Failure -> Result.Failure(LoaderError.CONNECTIVITY)
             is Result.Success -> {
                 val payload = result.data.toString(Charsets.UTF_8)
                 try {
-                    val photoResponse = Json.decodeFromString<List<PhotoResponse>>(payload)
-                    return Result.Success(listOf())
+                    val photosResponse = Json.decodeFromString<List<PhotoResponse>>(payload)
+                    val photos = photosResponse.map {
+                        Photo(
+                            id = it.id,
+                            author = it.author,
+                            width = it.width,
+                            height = it.height,
+                            webURL = it.url,
+                            imageURL = it.downloadURL
+                        )
+                    }
+                    return Result.Success(photos)
                 } catch (e: Exception) {
                     return Result.Failure(LoaderError.INVALID_DATA)
                 }
@@ -103,13 +155,39 @@ class RemotePhotoLoader(private val client: HTTPClientSpy, private val url: URL)
 }
 
 data class Photo(
-    val id: String
+    val id: String,
+    val author: String,
+    val width: Int,
+    val height: Int,
+    val webURL: URL,
+    val imageURL: URL
 )
 
 @Serializable
 data class PhotoResponse(
-    val id: String
+    val id: String,
+    val author: String,
+    val width: Int,
+    val height: Int,
+    @Serializable(with = URLSerializer::class)
+    val url: URL,
+    @SerialName("download_url")
+    @Serializable(with = URLSerializer::class)
+    val downloadURL: URL
 )
+
+object URLSerializer : KSerializer<URL> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("stringAsURLSerializer", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: URL) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): URL {
+        return URL(decoder.decodeString())
+    }
+}
 
 enum class HTTPClientError : Error {
     UNKNOWN
