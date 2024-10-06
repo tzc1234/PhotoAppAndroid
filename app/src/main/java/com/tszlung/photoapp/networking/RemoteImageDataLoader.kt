45059103b33eb2783baeeb05ff00f8dfea9ec13b
tzc1234 <peopli9997@gmail.com>
2024-10-06 18:54:39 +0100

    Clean up

--- app/src/main/java/com/tszlung/photoapp/networking/RemoteImageDataLoader.kt
+++ app/src/main/java/com/tszlung/photoapp/networking/RemoteImageDataLoader.kt
@@ -6,7 +6,7 @@ import com.tszlung.photoapp.util.*
 import java.net.URL
 
 class RemoteImageDataLoader(private val client: HTTPClient) : ImageDataLoader {
-    override suspend fun loadFrom(url: URL): com.tszlung.photoapp.util.Result<ByteArray, Error> {
+    override suspend fun loadFrom(url: URL): Result<ByteArray, Error> {
         return when(val result = client.getFrom(url)) {
             is Result.Failure -> Result.Failure(ImageDataLoaderError.CONNECTIVITY)
             is Result.Success -> {
