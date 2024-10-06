64ae041ad1f5465c7b5280769489eebfaa8952ec
tzc1234 <peopli9997@gmail.com>
2024-10-06 15:59:58 +0100

    Extract RemoteImageDataLoader to production

--- app/src/test/java/com/tszlung/photoapp/RemoteImageDataLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/RemoteImageDataLoaderTest.kt
@@ -4,10 +4,9 @@ import com.tszlung.photoapp.features.ImageDataLoader
 import com.tszlung.photoapp.features.ImageDataLoaderError
 import com.tszlung.photoapp.helpers.HTTPClientSpy
 import com.tszlung.photoapp.helpers.anyURL
-import com.tszlung.photoapp.networking.HTTPClient
 import com.tszlung.photoapp.networking.HTTPClientError
-import com.tszlung.photoapp.util.Error
-import com.tszlung.photoapp.util.Result
+import com.tszlung.photoapp.networking.RemoteImageDataLoader
+import com.tszlung.photoapp.util.*
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -74,19 +73,4 @@ class RemoteImageDataLoaderTest {
         return "any".toByteArray(Charsets.UTF_8)
     }
     // endregion
-}
-
-class RemoteImageDataLoader(private val client: HTTPClient) : ImageDataLoader {
-    override suspend fun loadFrom(url: URL): Result<ByteArray, Error> {
-        return when(val result = client.getFrom(url)) {
-            is Result.Failure -> Result.Failure(ImageDataLoaderError.CONNECTIVITY)
-            is Result.Success -> {
-                if (result.data.isEmpty()) {
-                    Result.Failure(ImageDataLoaderError.INVALID_DATA)
-                } else {
-                    Result.Success(result.data)
-                }
-            }
-        }
-    }
 }
