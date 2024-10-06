964a8db142553fdfaa819336b5322eb099a20a2c
tzc1234 <peopli9997@gmail.com>
2024-10-06 15:34:37 +0100

    Add helpers

--- app/src/test/java/com/tszlung/photoapp/RemoteImageDataLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/RemoteImageDataLoaderTest.kt
@@ -2,6 +2,7 @@ package com.tszlung.photoapp
 
 import com.tszlung.photoapp.helpers.HTTPClientSpy
 import com.tszlung.photoapp.networking.HTTPClient
+import com.tszlung.photoapp.util.Error
 import com.tszlung.photoapp.util.Result
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
@@ -12,24 +13,31 @@ class RemoteImageDataLoaderTest {
     @Test
     @Suppress("UNUSED_VARIABLE")
     fun `loader does not notify client upon init`() {
-        val anyData = "any".toByteArray(Charsets.UTF_8)
-        val client = HTTPClientSpy(Result.Success(anyData))
-        @Suppress("unused") val sut = RemoteImageDataLoader(client)
+        val (sut, client) = makSUT()
 
         assertTrue(client.messages.isEmpty())
     }
 
     @Test
     fun `load from URL`() = runBlocking {
-        val anyData = "any".toByteArray(Charsets.UTF_8)
-        val client = HTTPClientSpy(Result.Success(anyData))
-        val sut = RemoteImageDataLoader(client)
+        val (sut, client) = makSUT()
         val url = URL("https://a-url.com")
 
         sut.loadFrom(url)
 
         assertEquals(listOf(url), client.messages)
     }
+
+    // region Helpers
+    private fun makSUT(stub: Result<ByteArray, Error> = Result.Success(anyData())): Pair<RemoteImageDataLoader, HTTPClientSpy> {
+        val client = HTTPClientSpy(stub)
+        return Pair(RemoteImageDataLoader(client), client)
+    }
+
+    private fun anyData(): ByteArray {
+        return "any".toByteArray(Charsets.UTF_8)
+    }
+    // endregion
 }
 
 class RemoteImageDataLoader(private val client: HTTPClient) {
