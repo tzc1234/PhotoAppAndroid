a2a8c086551fe2981934d778bed8828198d1a1c1
tzc1234 <peopli9997@gmail.com>
2024-10-06 15:29:06 +0100

    RemoteImageDataLoader load from url

--- app/src/test/java/com/tszlung/photoapp/RemoteImageDataLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/RemoteImageDataLoaderTest.kt
@@ -3,8 +3,10 @@ package com.tszlung.photoapp
 import com.tszlung.photoapp.helpers.HTTPClientSpy
 import com.tszlung.photoapp.networking.HTTPClient
 import com.tszlung.photoapp.util.Result
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.net.URL
 
 class RemoteImageDataLoaderTest {
     @Test
@@ -16,6 +18,22 @@ class RemoteImageDataLoaderTest {
 
         assertTrue(client.messages.isEmpty())
     }
+
+    @Test
+    fun `load from URL`() = runBlocking {
+        val anyData = "any".toByteArray(Charsets.UTF_8)
+        val client = HTTPClientSpy(Result.Success(anyData))
+        val sut = RemoteImageDataLoader(client)
+        val url = URL("https://a-url.com")
+
+        sut.loadFrom(url)
+
+        assertEquals(listOf(url), client.messages)
+    }
 }
 
-class RemoteImageDataLoader(private val client: HTTPClient)
+class RemoteImageDataLoader(private val client: HTTPClient) {
+    suspend fun loadFrom(url: URL) {
+        client.getFrom(url)
+    }
+}
