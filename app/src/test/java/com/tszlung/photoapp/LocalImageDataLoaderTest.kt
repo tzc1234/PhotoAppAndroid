8cd2ec3e06770f277dc3f4c5df41cf9019b4f68b
tzc1234 <peopli9997@gmail.com>
2024-10-07 11:12:42 +0100

    LocalImageDataLoader delivers data not found when no cache

--- app/src/test/java/com/tszlung/photoapp/LocalImageDataLoaderTest.kt
+++ app/src/test/java/com/tszlung/photoapp/LocalImageDataLoaderTest.kt
@@ -1,10 +1,12 @@
-package com.tszlung
+package com.tszlung.photoapp
 
 import com.tszlung.photoapp.features.ImageDataLoader
 import com.tszlung.photoapp.util.Error
 import com.tszlung.photoapp.util.Result
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import java.net.URL
 
 class LocalImageDataLoaderTest {
@@ -17,6 +19,22 @@ class LocalImageDataLoaderTest {
         assertTrue(store.messages.isEmpty())
     }
 
+    @Test
+    fun `delivers data not found error when no cache`() = runBlocking {
+        val store = ImageDataStoreSpy()
+        val sut = LocalImageDataLoader(store = store)
+        val url = URL("https://a-url.com")
+
+        when (val result = sut.loadFrom(url)) {
+            is Result.Failure -> assertEquals(
+                LocalImageDataLoader.LoaderError.DATA_NOT_FOUND,
+                result.error
+            )
+
+            is Result.Success -> fail("should not be success")
+        }
+    }
+
     // region Helpers
     private class ImageDataStoreSpy : ImageDataStore {
         val messages = listOf<Any>()
@@ -27,7 +45,11 @@ class LocalImageDataLoaderTest {
 interface ImageDataStore
 
 class LocalImageDataLoader(private val store: ImageDataStore) : ImageDataLoader {
+    enum class LoaderError : Error {
+        DATA_NOT_FOUND
+    }
+
     override suspend fun loadFrom(url: URL): Result<ByteArray, Error> {
-        TODO("Not yet implemented")
+        return Result.Failure(LoaderError.DATA_NOT_FOUND)
     }
 }
