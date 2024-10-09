672efe5803e1eaa38a14f4592b80f5b37d9d1136
tzc1234 <peopli9997@gmail.com>
2024-10-09 16:06:16 +0100

    Improve the error message test coverage

--- app/src/test/java/com/tszlung/photoapp/viewModels/PhotosViewModelTests.kt
+++ app/src/test/java/com/tszlung/photoapp/viewModels/PhotosViewModelTests.kt
@@ -3,6 +3,7 @@ package com.tszlung.photoapp.viewModels
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tszlung.photoapp.features.Photo
@@ -13,6 +14,7 @@ import com.tszlung.photoapp.viewModels.helpers.MainCoroutineExtension
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -32,19 +34,18 @@ class PhotosViewModelTests {
 
     @Test
     fun `load photos delivers isLoading correctly on loader success`() = runTest {
-        val sut = makeSUT(Result.Success(listOf()))
+        val sut = makeSUT(mutableListOf(Result.Success(listOf())))
 
         sut.loadPhotos()
         assertTrue(sut.isLoading)
 
         advanceUntilIdle()
-
         assertFalse(sut.isLoading)
     }
 
     @Test
     fun `load photos delivers isLoading correctly on loader failure`() = runTest {
-        val sut = makeSUT(Result.Success(listOf()))
+        val sut = makeSUT(mutableListOf(Result.Success(listOf())))
 
         sut.loadPhotos()
         assertTrue(sut.isLoading)
@@ -54,30 +55,34 @@ class PhotosViewModelTests {
     }
 
     @Test
-    fun `load photos delivers empty photos when received empty photos`() = runTest {
-        val sut = makeSUT(Result.Success(listOf()))
-
-        sut.loadPhotos()
-        assertTrue(sut.photos.isEmpty())
-
-        advanceUntilIdle()
-        assertTrue(sut.photos.isEmpty())
-    }
-
-    @Test
     fun `load photos delivers error message when received error from loader`() = runTest {
-        val sut = makeSUT(Result.Failure(LoaderError.ANY))
+        val sut = makeSUT(mutableListOf(Result.Failure(LoaderError.ANY), Result.Success(listOf())))
 
         sut.loadPhotos()
         assertNull(sut.errorMessage)
 
         advanceUntilIdle()
         assertEquals("Error occurred, please try again.", sut.errorMessage)
+
+        sut.loadPhotos()
+        advanceUntilIdle()
+        assertNull(sut.errorMessage)
+    }
+
+    @Test
+    fun `load photos delivers empty photos when received empty photos`() = runTest {
+        val sut = makeSUT(mutableListOf(Result.Success(listOf())))
+
+        sut.loadPhotos()
+        assertTrue(sut.photos.isEmpty())
+
+        advanceUntilIdle()
+        assertTrue(sut.photos.isEmpty())
     }
 
     // region Helpers
-    private fun makeSUT(stub: Result<List<Photo>, Error> = Result.Success(listOf())): PhotosViewModel {
-        val photosLoader = PhotosLoaderStub(stub)
+    private fun makeSUT(stubs: MutableList<Result<List<Photo>, Error>> = mutableListOf<Result<List<Photo>, Error>>()): PhotosViewModel {
+        val photosLoader = PhotosLoaderStub(stubs)
         return PhotosViewModel(photosLoader)
     }
 
@@ -85,9 +90,11 @@ class PhotosViewModelTests {
         ANY
     }
 
-    private class PhotosLoaderStub(private val stub: Result<List<Photo>, Error>) : PhotosLoader {
+    private class PhotosLoaderStub(val stubs: MutableList<Result<List<Photo>, Error>>) :
+        PhotosLoader {
         override suspend fun load(): Result<List<Photo>, Error> {
-            return stub
+            val result = stubs.removeFirst()
+            return result
         }
     }
     // endregion
@@ -106,7 +113,10 @@ class PhotosViewModel(private val loader: PhotosLoader) : ViewModel() {
         viewModelScope.launch {
             when (val result = loader.load()) {
                 is Result.Failure -> errorMessage = "Error occurred, please try again."
-                is Result.Success -> photos = result.data
+                is Result.Success -> {
+                    photos = result.data
+                    errorMessage = null
+                }
             }
 
             isLoading = false
