013693d7e42571cba4c116f9cdbefcfbe5a3f72f
tzc1234 <peopli9997@gmail.com>
2024-10-14 14:21:50 +0100

    Extract ImageDataLoaderWithFallbackComposite to production

--- app/src/test/java/com/tszlung/photoapp/main/ImageDataLoaderWithFallbackCompositeTests.kt
+++ app/src/test/java/com/tszlung/photoapp/main/ImageDataLoaderWithFallbackCompositeTests.kt
@@ -63,15 +63,3 @@ class ImageDataLoaderWithFallbackCompositeTests {
     }
     // endregion
 }
-
-class ImageDataLoaderWithFallbackComposite(
-    private val primary: ImageDataLoader,
-    private val fallback: ImageDataLoader
-) : ImageDataLoader {
-    override suspend fun loadFrom(url: URL): Result<ByteArray, Error> {
-        return when (val result = primary.loadFrom(url)) {
-            is Result.Failure -> fallback.loadFrom(url)
-            is Result.Success -> result
-        }
-    }
-}
