41ede6084218070ce546e999f9ce7fd6fadb3eb9
tzc1234 <peopli9997@gmail.com>
2024-10-15 10:14:00 +0100

    PhotoDetailViewModel loadImage requests url from loader

--- app/src/test/java/com/tszlung/photoapp/presentation/PhotoDetailViewModelTests.kt
+++ app/src/test/java/com/tszlung/photoapp/presentation/PhotoDetailViewModelTests.kt
@@ -1,16 +1,24 @@
 package com.tszlung.photoapp.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tszlung.photoapp.features.ImageDataLoader
 import com.tszlung.photoapp.features.Photo
 import com.tszlung.photoapp.helpers.AnyError
 import com.tszlung.photoapp.helpers.makePhoto
+import com.tszlung.photoapp.presentation.helpers.MainCoroutineExtension
 import com.tszlung.photoapp.util.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import java.net.URL
 
+@ExtendWith(MainCoroutineExtension::class)
+@OptIn(ExperimentalCoroutinesApi::class)
 class PhotoDetailViewModelTests {
     @Test
     fun `init all properties properly upon creation`() {
@@ -20,6 +28,17 @@ class PhotoDetailViewModelTests {
         assertEquals(photo.author, sut.author)
         assertEquals(photo.webURL, sut.webURL)
         assertTrue(loader.requestURLs.isEmpty())
+    }
+
+    @Test
+    fun `loadImage requests url from loader`() = runTest {
+        val photo = makePhoto(0)
+        val (sut, loader) = makeSUT(photo)
+
+        sut.loadImage()
+        advanceUntilIdle()
+
+        assertEquals(listOf(photo.imageURL), loader.requestURLs)
     }
 
     // region Helpers
@@ -50,4 +69,10 @@ class PhotoDetailViewModel(private val photo: Photo, private val loader: ImageDa
         get() = photo.author
     val webURL: URL
         get() = photo.webURL
+
+    fun loadImage() {
+        viewModelScope.launch {
+            loader.loadFrom(photo.imageURL)
+        }
+    }
 }
