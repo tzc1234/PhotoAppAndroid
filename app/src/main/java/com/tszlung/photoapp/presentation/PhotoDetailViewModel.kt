1a7da5bf30ba84a6da766c476298256066a3a7cd
tzc1234 <peopli9997@gmail.com>
2024-10-15 10:37:04 +0100

    Extract PhotoDetailViewModel to production

---
+++ app/src/main/java/com/tszlung/photoapp/presentation/PhotoDetailViewModel.kt
@@ -0,0 +1,36 @@
+package com.tszlung.photoapp.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tszlung.photoapp.features.ImageDataLoader
+import com.tszlung.photoapp.features.Photo
+import com.tszlung.photoapp.util.Result
+import kotlinx.coroutines.launch
+
+class PhotoDetailViewModel<I>(
+    private val photo: Photo,
+    private val loader: ImageDataLoader,
+    private val imageConvertor: (ByteArray) -> I?
+) : ViewModel() {
+    val author get() = photo.author
+    val webURL get() = photo.webURL
+    var isLoading by mutableStateOf(false)
+        private set
+    var image by mutableStateOf<I?>(null)
+        private set
+
+    fun loadImage() {
+        isLoading = true
+        viewModelScope.launch {
+            when (val result = loader.loadFrom(photo.imageURL)) {
+                is Result.Failure -> Unit
+                is Result.Success -> image = imageConvertor(result.data)
+            }
+
+            isLoading = false
+        }
+    }
+}
--- app/src/test/java/com/tszlung/photoapp/presentation/PhotoDetailViewModelTests.kt
+++ app/src/test/java/com/tszlung/photoapp/presentation/PhotoDetailViewModelTests.kt
@@ -1,7 +1,5 @@
 package com.tszlung.photoapp.presentation
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.tszlung.photoapp.features.ImageDataLoader
 import com.tszlung.photoapp.features.Photo
 import com.tszlung.photoapp.helpers.AnyError
@@ -9,11 +7,7 @@ import com.tszlung.photoapp.helpers.anyData
 import com.tszlung.photoapp.helpers.makePhoto
 import com.tszlung.photoapp.presentation.helpers.MainCoroutineExtension
 import com.tszlung.photoapp.util.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
@@ -125,30 +119,4 @@ class PhotoDetailViewModelTests {
         }
     }
     // endregion
-}
-
-class PhotoDetailViewModel<I>(
-    private val photo: Photo,
-    private val loader: ImageDataLoader,
-    private val imageConvertor: (ByteArray) -> I?
-) :
-    ViewModel() {
-    val author get() = photo.author
-    val webURL get() = photo.webURL
-    var isLoading by mutableStateOf(false)
-        private set
-    var image by mutableStateOf<I?>(null)
-        private set
-
-    fun loadImage() {
-        isLoading = true
-        viewModelScope.launch {
-            when (val result = loader.loadFrom(photo.imageURL)) {
-                is Result.Failure -> Unit
-                is Result.Success -> image = imageConvertor(result.data)
-            }
-
-            isLoading = false
-        }
-    }
 }
